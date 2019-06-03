15b061ded9a0fd0ed85384ba864985355eb63b26
tejaswinee <tejaswinee@Abhinavs-MacBook-Pro-3.local>
2019-06-03 16:42:56 +0530

    initial cucumber and eclipse set up along with task user flow feature file

---
+++ src/test/java/CasparHealthBDD/Stepdefs.java
@@ -0,0 +1,52 @@
+package CasparHealthBDD;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefs {
+	@Given("Navigate to \\{ login_page_link }")
+	public void navigate_to() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Given("Login with the following user credentials \\{ casparID } and \\{ password }")
+	public void login_with_the_following_user_credentials_and() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("Click “Add patient” and create a new patient")
+	public void click_Add_patient_and_create_a_new_patient() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("Use the login credentials of the patient")
+	public void use_the_login_credentials_of_the_patient() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("Logout the user")
+	public void logout_the_user() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("Login as the new patient on the same url like provided in step {int}")
+	public void login_as_the_new_patient_on_the_same_url_like_provided_in_step(Integer int1) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Then("TandC and release of medical information page is displayed")
+	public void tandc_and_release_of_medical_information_page_is_displayed() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+
+
+}
---
+++ src/test/resources/CasparHealthBDD/FunctionalTests/task_flow.feature
@@ -0,0 +1,12 @@
+Feature: Display of Terms and Conditions and Release of Medical Information on first login
+	On first Login, users must know the Terms of service.
+  
+
+  Scenario: Creating new patient from therapist dashboard, logging in with new credentials and checking terms of service on first login
+    Given  Navigate to { login_page_link }
+    And Login with the following user credentials { casparID } and { password }
+    When Click “Add patient” and create a new patient
+    And Use the login credentials of the patient
+    And Logout the user
+    And Login as the new patient on the same url like provided in step 1
+    Then  TandC and release of medical information page is displayed
